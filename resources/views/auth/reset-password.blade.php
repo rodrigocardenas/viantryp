6bd6ef53e163b01fd4d4734dfb77d9d13fe928f6
GPSVIAJES <gpsviajes@gmail.com>
2026-03-19 17:46:55 -0500

    fix: remove orphaned code and endsection from reset-password.blade.php

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -379,12 +379,6 @@
         input.type = 'password';
         icon.innerHTML = '<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/><circle cx="12" cy="12" r="3"/>';
       }
-    }
   </script>
 </body>
 </html>
-.add('fa-eye');
-    }
-}
-</script>
-@endsection
