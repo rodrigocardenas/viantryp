17794f083dfb84b0c3ef9c22d74e7a5faa9f3731
GPSVIAJES <gpsviajes@gmail.com>
2026-09-21 19:17:42 -0500

    Optimize onboarding images to 3K Ultra-HD with smooth anti-aliased rendering

--- resources/views/components/onboarding-carousel.blade.php
+++ resources/views/components/onboarding-carousel.blade.php
@@ -82,7 +82,7 @@
   .slide-3 { background: #d1e5ee; }
   .slide-4 { background: #3889a6; }
 
-  /* Clean PNG Image Container - High-Res Crisp Rendering */
+  /* Clean PNG Image Container - Smooth 3K Ultra-HD Rendering */
   .onboarding-img {
     width: 100%;
     height: 100%;
@@ -91,8 +91,11 @@
     display: block;
     user-select: none;
     -webkit-user-drag: none;
-    image-rendering: -webkit-optimize-contrast;
-    image-rendering: crisp-edges;
+    image-rendering: smooth;
+    image-rendering: high-quality;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    transform: translateZ(0);
   }
 
   /* Bottom Controls & Action Overlay */
@@ -226,22 +229,22 @@
       <div class="swiper-wrapper">
         <!-- Slide 1 -->
         <div class="swiper-slide slide-1">
-          <img src="{{ asset('images/onboarding/app1.png') }}?v=20260921_2k_hd" alt="Viantryp" class="onboarding-img" loading="eager">
+          <img src="{{ asset('images/onboarding/app1.png') }}?v=20260921_3k_smooth" alt="Viantryp" class="onboarding-img" loading="eager">
         </div>
 
         <!-- Slide 2 -->
         <div class="swiper-slide slide-2">
-          <img src="{{ asset('images/onboarding/app2.png') }}?v=20260921_2k_hd" alt="Diseña tus viajes en cuestión de minutos" class="onboarding-img" loading="eager">
+          <img src="{{ asset('images/onboarding/app2.png') }}?v=20260921_3k_smooth" alt="Diseña tus viajes en cuestión de minutos" class="onboarding-img" loading="eager">
         </div>
 
         <!-- Slide 3 -->
         <div class="swiper-slide slide-3">
-          <img src="{{ asset('images/onboarding/app3.png') }}?v=20260921_2k_hd" alt="Plasma tu viaje en solo 3 pasos" class="onboarding-img" loading="eager">
+          <img src="{{ asset('images/onboarding/app3.png') }}?v=20260921_3k_smooth" alt="Plasma tu viaje en solo 3 pasos" class="onboarding-img" loading="eager">
         </div>
 
         <!-- Slide 4 -->
         <div class="swiper-slide slide-4">
-          <img src="{{ asset('images/onboarding/app4.png') }}?v=20260921_2k_hd" alt="Todo lo que necesitas en un solo lugar" class="onboarding-img" loading="eager">
+          <img src="{{ asset('images/onboarding/app4.png') }}?v=20260921_3k_smooth" alt="Todo lo que necesitas en un solo lugar" class="onboarding-img" loading="eager">
         </div>
       </div>
     </div>
