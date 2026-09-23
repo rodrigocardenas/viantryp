44fffacbfa3f5084e86c55ab8f130267bac1a4ea
GPSVIAJES <gpsviajes@gmail.com>
2026-09-23 17:28:43 -0500

    chore: bump release to version 21 (1.0.9) and clean up legacy scripts

--- list_air_prod.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require '/var/www/html/viantryp/vendor/autoload.php';
-$app = require_once '/var/www/html/viantryp/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-$list = DB::table('airlines')->select('id','name','country','carrier_code')->orderBy('name')->get();
-foreach($list as $a) {
-    echo "[" . $a->id . "] " . $a->name . " (" . ($a->country ?? 'NULL') . ") [" . ($a->carrier_code ?? 'NULL') . "]" . PHP_EOL;
-}
